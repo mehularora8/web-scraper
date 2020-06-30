d90ff21bb77d718cd3fcd8d5d273b91bb2188a8a
Mehul Arora <aroramehul8@gmail.com>
2020-06-29 17:26:59 -0700

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,10 +1,13 @@
 import json
 
+# Get JSON data from file
 with open("JFtweets.json") as fin:
 	tweet_data = json.load(fin)
 
+# Counter
 jimmyCounter = 0
 
+# Find words
 for tweet in tweet_data:
 	if "obama" in tweet['tweet'].lower():
 		jimmyCounter += 1
